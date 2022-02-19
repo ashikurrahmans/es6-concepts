9f765012adb3210e20a640fdb50f7c4b5d76ff09
HackBomm <hackbomm.net@gmail.com>
2022-02-19 22:49:25 +0600

    map function concept

--- map.js
+++ map.js
@@ -1,3 +1,5 @@
+
+// Map Function using Array 
 const numbers = [3,45,76,4,34,56 ]
 const arr = []
 
@@ -5,4 +7,30 @@ for(const number of numbers){
     const numbers = number
     arr.push(numbers)
 }
-console.log(arr);
+// console.log(arr);
+
+// Traditional Function
+
+function makeItDouble(number){
+    return number * 2
+}
+
+// makeItDouble(2)
+
+// Arrow Function 
+
+const doubleIt = number => number * 2
+// console.log(doubleIt(2));
+
+
+// Iterte to a array using map funciton 
+
+const arrForMap = [1,2,6,5,2,7,4,8]
+const outputs = arrForMap.map(number =>number * 2)
+// console.log(outputs);
+
+
+const seiArr = [20,43,5,30,44,2,44,5]
+
+const tomarArr = seiArr.map(n => n * n)
+console.log(tomarArr);
