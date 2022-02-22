058ebb436bc408b380f4683cc36c14be11f7aada
HackBomm <hackbomm.net@gmail.com>
2022-02-22 11:09:35 +0600

    Post

--- json.js
+++ json.js
@@ -11,6 +11,31 @@ function loadUser(){
 
 }
 
+function posts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(posts => displayPost(posts))
+
+}
+posts()
+function displayPost(posts){
+   const containers = document.getElementById("posts")
+    for(const post of posts){
+        const createDiv = document.createElement('div')
+        createDiv.innerHTML = `
+        <h2>${post.title}</h2>
+        <p>${post.body}</p>
+        `
+        containers.appendChild(createDiv);
+        console.log(post);
+ 
+    }
+}
+
+
+
+
+
 function displayUser(data){
     const ul = document.getElementById('createUser');
     for(const user of data){
